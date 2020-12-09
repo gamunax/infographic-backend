ef890a4cb03bcf3c40bc84ad61dfd9b9bd4249ac
Jan Pierre <gamunaxx@gmail.com>
2020-12-09 18:09:28 -0500

    feat: add paths

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
+  url: 'https://infographic.dev/api',
   port: env.int('PORT', 1337),
   admin: {
+    url: 'http://infographic.dev/dashboard',
     auth: {
       secret: "${process.env.ADMIN_JWT_SECRET}"
     },
