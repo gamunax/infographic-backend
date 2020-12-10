8482fb53752697063089811bd1edbfe9b971ab79
Jan Pierre <gamunaxx@gmail.com>
2020-12-10 01:48:27 -0500

    changes

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  url: 'https://infographic.dev/api/',
+  url: 'https://infographic.dev/api',
   proxy: true,
   port: env.int('PORT', 1337),
   admin: {
-    url: 'https://infographic.dev/dashboard/',
+    url: 'https://infographic.dev/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET')
     }
