2d14f1820ab2f7aac2b5d5295ed345de521108fe
Jan Pierre <gamunaxx@gmail.com>
2020-12-10 00:51:37 -0500

    rollback url

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,9 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  // url: 'https://infographic.dev/api',
+  url: 'https://infographic.dev/api',
   port: env.int('PORT', 1337),
   admin: {
-    path: 'https://infographic.dev/dashboard',
     auth: {
       secret: "${process.env.ADMIN_JWT_SECRET}"
     },
