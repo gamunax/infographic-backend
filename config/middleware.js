ccbe66851b8070ebfaaae9fb1f740218b7b8d21c
Jan Pierre <gamunaxx@gmail.com>
2020-12-31 18:53:46 -0500

    feat: update middleware

--- config/middleware.js
+++ config/middleware.js
@@ -2,11 +2,11 @@ module.exports = {
   settings: {
     cors: {
       origin: [
-        'http://localhost:4200', 
-        'http://localhost:3000', 
+        'http://localhost:5811', 
+        'http://localhost:3000',
         'https://infographic.dev',
+        'https://www.infographic.dev',
         'http://159.65.237.96',
-        'https://strapi-infografia.herokuapp.com'
       ]
     }
   }
