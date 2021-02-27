2c8aa885b9ace78e74b2182b7bf023cf8a51a37d
Jan Pierre <gamunaxx@gmail.com>
2021-02-27 01:30:03 -0500

    feat: update

--- config/env/development/server.js
+++ config/env/development/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 4000),
   admin: {
+    url: '/dashboard',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '36bafbed4fcb12724bec6d18a94c70cxb'),
     },
