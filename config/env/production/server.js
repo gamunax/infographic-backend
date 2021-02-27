8fe36b11e226e19a0ba15411bd99962be1626d21
Jan Pierre <gamunaxx@gmail.com>
2021-02-26 20:33:05 -0500

    feat: upgrade strapi

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '127.0.0.1'),
+  host: env('HOST', '0.0.0.0'),
   url: 'https://infographic.dev/api',
   proxy: true,
   port: env.int('PORT', 1337),
