c848268b3bd4060afc4c003548b5d594f1f78ca8
Jan Pierre <gamunaxx@gmail.com>
2021-03-01 18:44:46 -0500

    feat: change path url

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   proxy: true,
   port: env.int('PORT', 1337),
   admin: {
-    url: '/admin',
+    url: 'https://infographic.dev/admin',
     auth: {
       secret: env('ADMIN_JWT_SECRET')
     },
